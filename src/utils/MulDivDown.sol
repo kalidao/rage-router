7e6b3e553db9de282057d37e11a5a76500e555f2
z0r0z <zrzr8963@gmail.com>
2022-10-18 07:50:59 +0000

    ⚡️ Optimize gas consumption for mulDivDown

--- src/utils/MulDivDown.sol
+++ src/utils/MulDivDown.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+uint256 constant MAX_UINT256 = 2**256 - 1;
+
 /// @dev Returns `floor(x * y / denominator)`.
 /// Reverts if `x * y` overflows, or `denominator` is zero.
 function mulDivDown(
@@ -10,12 +12,11 @@ function mulDivDown(
 ) pure returns (uint256 z) {
     assembly {
         // Equivalent to require(denominator != 0 && (y == 0 || x <= type(uint256).max / y))
-        if iszero(mul(denominator, iszero(mul(y, gt(x, div(not(0), y)))))) {
-            // Store the function selector of `MulDivFailed()`.
-            mstore(0x00, 0xad251c27)
-            // Revert with (offset, size).
-            revert(0x1c, 0x04)
+        if iszero(mul(denominator, iszero(mul(y, gt(x, div(MAX_UINT256, y)))))) {
+            revert(0, 0)
         }
+
+        // Divide x * y by the denominator.
         z := div(mul(x, y), denominator)
     }
 }
